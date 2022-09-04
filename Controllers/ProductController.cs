8a588a1523cd2fff708d225b3c68cb374441bd74
matt-cassinelli <67283034+matt-cassinelli@users.noreply.github.com>
2022-09-05 00:01:45 +0100

    Add controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+
+namespace Bakery.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;   // Constructor injection.
+            _categoryRepository = categoryRepository; //
+        }
+
+        public IActionResult List()
+        {
+            return View(_productRepository.AllProducts);
+        }
+    }
+}
